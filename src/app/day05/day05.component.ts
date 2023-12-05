7f24cd799dd07b8d5645f1387103a942b40b0233
alexander moeller <alexander.moeller@posteo.de>
2023-12-05 23:15:00 +0100

    Day 5 Part 1 solved. Erst Schulung und dann direkt im Anschluss Tischtennis-Training. War geil aber danach hats nur noch für Part 1 gereicht.

--- src/app/day05/day05.component.ts
+++ src/app/day05/day05.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, SimpleChange } from '@angular/core';
 import { DataService } from '../data-service.service';
-import { parseData } from './helperFunctions';
+import { AlmanacCategory, Ranges, SimpleRange, calcValueOfLevel_Part1, myType, parseData } from './helperFunctions';
 
 @Component({
   selector: 'app-day05',
@@ -8,23 +8,174 @@ import { parseData } from './helperFunctions';
   styleUrls: ['./day05.component.scss'],
 })
 export class Day05Component {
-  testData1: string = "";
-  testData2: string = "";
-  realData: string = "";
+  testData1: string = '';
+  testData2: string = '';
+  realData: string = '';
 
   resultTest1 = 0;
   resultTest2 = 0;
   resultSolved1 = 0;
   resultSolved2 = 0;
 
-
-
   solvePuzzlePart_1(data: string): number {
-    parseData(data);
-    return 1;
+    const parsedData = parseData(data);
+    const seeds = parsedData.seeds;
+    const seedsAfterLvl1 = calcValueOfLevel_Part1(seeds, parsedData.categories[0].entries);
+    const seedsAfterLvl2 = calcValueOfLevel_Part1(seedsAfterLvl1, parsedData.categories[1].entries);
+    const seedsAfterLvl3 = calcValueOfLevel_Part1(seedsAfterLvl2, parsedData.categories[2].entries);
+    const seedsAfterLvl4 = calcValueOfLevel_Part1(seedsAfterLvl3, parsedData.categories[3].entries);
+    const seedsAfterLvl5 = calcValueOfLevel_Part1(seedsAfterLvl4, parsedData.categories[4].entries);
+    const seedsAfterLvl6 = calcValueOfLevel_Part1(seedsAfterLvl5, parsedData.categories[5].entries);
+    const seedsAfterLvl7 = calcValueOfLevel_Part1(seedsAfterLvl6, parsedData.categories[6].entries);
+    return Math.min(...seedsAfterLvl7);
   }
 
   solvePuzzlePart_2(data: string): number {
+    const parsedData = parseData(data);
+    const seeds = parsedData.seeds;
+
+    const entriesFirstLevel = parsedData.categories[0].entries;
+
+    const ranges: Ranges[] = [];
+
+    const myRanges: myType[] = [];
+
+    let rangesFromLastLevelToLookAt: SimpleRange[] = [
+      { start: seeds[0], end: seeds[0] + seeds[1] - 1 },
+      { start: seeds[2], end: seeds[2] + seeds[3] - 1 },
+    ];
+    const restRangesToLookAt: SimpleRange[] = [];
+    console.log(entriesFirstLevel);
+
+    do {
+      for (let range of rangesFromLastLevelToLookAt) {
+        let rangeHadAHit = false;
+        for (let entry of entriesFirstLevel) {
+          if (range.start >= entry.sourceRangeStart && range.end <= entry.sourceRangeStart + entry.rangeLength) {
+            // ranges.push({
+            //   sourceRangeStart: range.start,
+            //   destinationRangeStart: entry.destinationRangeStart + (range.start - entry.sourceRangeStart),
+            //   rangeLength: range.end - range.start,
+            // });
+            const newmyType = {
+              start: entry.destinationRangeStart + (range.start - entry.sourceRangeStart),
+              end: entry.destinationRangeStart + (range.start - entry.sourceRangeStart) + (range.end - range.start),
+            };
+            myRanges.push({
+              rangeFromLastLevel: range,
+              mappedToDestinationRange: newmyType,
+            });
+            console.log(
+              `Range from last level ${range.start} ${range.end}. It is completely in sourceRange ${
+                entry.sourceRangeStart
+              } ${entry.sourceRangeStart + entry.rangeLength} and mapps to ${newmyType.start} - ${newmyType.end}`
+            );
+            rangeHadAHit = true;
+            break;
+          }
+
+          if (
+            range.start >= entry.sourceRangeStart &&
+            range.start <= entry.sourceRangeStart + entry.rangeLength &&
+            range.end > entry.sourceRangeStart + entry.rangeLength
+          ) {
+            console.log(
+              `Range from last level ${range.start} ${range.end}. The start is in sourceRange ${
+                entry.sourceRangeStart
+              } ${entry.sourceRangeStart + entry.rangeLength}`
+            );
+            // ranges.push({
+            //   sourceRangeStart: range.start,
+            //   destinationRangeStart:
+            //     entry.destinationRangeStart + (entry.sourceRangeStart + entry.rangeLength - range.start),
+            //   rangeLength: range.start - entry.sourceRangeStart,
+            // });
+
+            const newmyType = {
+              start: entry.destinationRangeStart + (range.start - entry.sourceRangeStart),
+              end: entry.destinationRangeStart + (range.start - entry.sourceRangeStart) + (range.end - range.start),
+            };
+            myRanges.push({
+              rangeFromLastLevel: range,
+              mappedToDestinationRange: newmyType,
+            });
+
+            restRangesToLookAt.push({
+              start: entry.sourceRangeStart + entry.rangeLength,
+              end: range.end,
+            });
+
+            rangeHadAHit = true;
+            break;
+          }
+
+          if (
+            range.start < entry.sourceRangeStart &&
+            range.end > entry.sourceRangeStart &&
+            range.end < entry.sourceRangeStart + entry.rangeLength
+          ) {
+            console.log(
+              `Range from last level ${range.start} ${range.end}. The End is in sourceRange ${entry.sourceRangeStart} ${
+                entry.sourceRangeStart + entry.rangeLength
+              }`
+            );
+            ranges.push({
+              sourceRangeStart: entry.sourceRangeStart,
+              destinationRangeStart: entry.destinationRangeStart,
+              rangeLength: range.end - entry.sourceRangeStart,
+            });
+
+            restRangesToLookAt.push({
+              start: range.start,
+              end: entry.sourceRangeStart,
+            });
+
+            rangeHadAHit = true;
+            break;
+          }
+
+          if (range.start < entry.sourceRangeStart && range.end > entry.sourceRangeStart + entry.rangeLength) {
+            console.log(
+              `Range from last level ${range.start} ${range.end}. sourceRange is completely in Range ${
+                entry.sourceRangeStart
+              } ${entry.sourceRangeStart + entry.rangeLength}`
+            );
+            ranges.push({
+              sourceRangeStart: entry.sourceRangeStart,
+              destinationRangeStart: entry.destinationRangeStart,
+              rangeLength: entry.rangeLength,
+            });
+
+            restRangesToLookAt.push({
+              start: entry.sourceRangeStart + entry.rangeLength,
+              end: range.end,
+            });
+
+            restRangesToLookAt.push({
+              start: range.start,
+              end: entry.sourceRangeStart,
+            });
+
+            rangeHadAHit = true;
+          }
+        }
+
+        if (!rangeHadAHit) {
+          console.log(`Range from last level ${range.start} ${range.end}. Direct Mapping`);
+          ranges.push({
+            sourceRangeStart: range.start,
+            destinationRangeStart: range.start,
+            rangeLength: range.end - range.start,
+          });
+        }
+      }
+
+      rangesFromLastLevelToLookAt = [...restRangesToLookAt.splice(0)];
+    } while (rangesFromLastLevelToLookAt.length > 0);
+
+    console.log('erster durchlauf fertig. Ranges ');
+    console.log(ranges);
+
     return 1;
   }
 
--- src/app/day05/helperFunctions.ts
+++ src/app/day05/helperFunctions.ts
@@ -2,12 +2,8 @@ import { parseNumbersFromString } from '../globalHelpers';
 
 export function parseData(data: string): { seeds: number[]; categories: AlmanacCategory[] } {
   // const cardsToReturn: Card[] = [];
-  const toReturn: { seeds: number[]; categories: AlmanacCategory[] } = {
-    seeds: [],
-    categories: [],
-  };
-
   const dataSplitInBlocks = data.split(/\n\s*\n/);
+
   const seeds = dataSplitInBlocks[0]
     .split(':')[1]
     .trim()
@@ -15,33 +11,69 @@ export function parseData(data: string): { seeds: number[]; categories: AlmanacC
     .map((str) => parseNumbersFromString(str))
     .reduce((acc, curr) => acc.concat(curr), []);
 
-  const seedToSoilData = dataSplitInBlocks[1];
-  const seedToSoilEntries = seedToSoilData
+  const toReturn: { seeds: number[]; categories: AlmanacCategory[] } = {
+    seeds: seeds,
+    categories: [],
+  };
+
+  toReturn.categories.push(createCategoryFromData(dataSplitInBlocks[1], Attributes.SEED, Attributes.SOIL));
+  toReturn.categories.push(createCategoryFromData(dataSplitInBlocks[2], Attributes.SOIL, Attributes.FERTILIZER));
+  toReturn.categories.push(createCategoryFromData(dataSplitInBlocks[3], Attributes.FERTILIZER, Attributes.WATER));
+  toReturn.categories.push(createCategoryFromData(dataSplitInBlocks[4], Attributes.WATER, Attributes.LIGHT));
+  toReturn.categories.push(createCategoryFromData(dataSplitInBlocks[5], Attributes.LIGHT, Attributes.TEMPERATURE));
+  toReturn.categories.push(createCategoryFromData(dataSplitInBlocks[6], Attributes.TEMPERATURE, Attributes.HUMIDITY));
+  toReturn.categories.push(createCategoryFromData(dataSplitInBlocks[7], Attributes.HUMIDITY, Attributes.LOCATION));
+
+  return toReturn;
+}
+
+export function calcValueOfLevel_Part1(source: number[], entries: Ranges[]): number[] {
+  const toReturn: number[] = [];
+  for (let sourceValue of source) {
+    // get value from level1
+    let levelValue = 0;
+    let foundMapping = false;
+    for (let entry of entries) {
+      // is it part of the entry?
+      if (sourceValue >= entry.sourceRangeStart && sourceValue <= entry.sourceRangeStart + entry.rangeLength) {
+        // found it
+        const howFarIsSourceValueFromSourceStart = sourceValue - entry.sourceRangeStart;
+        levelValue = entry.destinationRangeStart + howFarIsSourceValueFromSourceStart;
+        foundMapping = true;
+        break;
+      }
+    }
+    if (!foundMapping) {
+      levelValue = sourceValue;
+    }
+    toReturn.push(levelValue);
+  }
+
+  return toReturn;
+}
+
+function createCategoryFromData(str: string, sourceType: Attributes, destinantionType: Attributes): AlmanacCategory {
+  const entries = str
     .split(':')[1]
     .trim()
     .split('\r\n')
     .map((str) => parseNumbersFromString(str));
 
-  toReturn.categories.push({
-    source: Attributes.SEED,
-    destination: Attributes.SOIL,
-    entries: seedToSoilEntries.map((numbers: number[]) => {
+  return {
+    source: sourceType,
+    destination: destinantionType,
+    entries: entries.map((numbers: number[]) => {
       return mapToEntry(numbers);
     }),
-  } as AlmanacCategory);
-
-  console.log(seedToSoilData);
-  console.log(seedToSoilEntries);
-
-  return toReturn;
+  } as AlmanacCategory;
 }
 
 function mapToEntry(numbers: number[]) {
   return {
-    sourceRangeStart: numbers[0],
-    destinationRangeStart: numbers[1],
-    rangeLength: numbers[3],
-  } as AlmanacEntry;
+    destinationRangeStart: numbers[0],
+    sourceRangeStart: numbers[1],
+    rangeLength: numbers[2],
+  } as Ranges;
 }
 
 export enum Attributes {
@@ -62,11 +94,22 @@ export type parsedData = {
 export type AlmanacCategory = {
   source: Attributes;
   destination: Attributes;
-  entries: AlmanacEntry[];
+  entries: Ranges[];
 };
 
-export type AlmanacEntry = {
+export type Ranges = {
   sourceRangeStart: number;
   destinationRangeStart: number;
   rangeLength: number;
 };
+
+export type SimpleRange = {
+  start: number,
+  end: number
+}
+
+export type myType = {
+  rangeFromLastLevel: SimpleRange,
+  mappedToDestinationRange: SimpleRange
+
+}
